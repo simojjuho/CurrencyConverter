1e483e4b917ba8175ad82a53f414df468e0b031f
Juho Simojoki <juho.simojoki@metropolia.fi>
2023-01-06 21:25:55 +0200

    API works fine now, just the actual functionality is needed. And styles!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import currencyService from './services/currencyService'
 
+const App = () => {
+const [currencyRates, setCurrencyRates] = useState([])
+const [ currencies, setCurrencies] = useState([])
 
-function App() {
-  return 
+useEffect(() => {
+  currencyService.getCurrencyRates()
+    .then(rates => {
+      setCurrencyRates(rates)
+    })
+
+  currencyService.getCurrencies()
+    .then(currencies => {
+      setCurrencies(currencies)
+    })
+}, [])
+ 
+
+return (
+  <div>
+    <h1>Currency rates app v0.1</h1>
+  </div>
+)
 }
 
 export default App;
--- src/services/currencyService.js
+++ src/services/currencyService.js
@@ -1,6 +1,16 @@
 import axios from 'axios'
+const { REACT_APP_CURRENCY_API_KEY } = process.env
 
-async function getAllCurrencies() {
-    const currencies = await axios.get('https://api.currencyfreaks.com/latest?apikey=')
-    return currencies
+const getCurrencyRates = async () => {
+    const response = await axios.get(`https://api.currencyfreaks.com/latest?apikey=${REACT_APP_CURRENCY_API_KEY}`)
+    return response.data
 }
+
+const getCurrencies = async () => {
+    const response = await axios.get('https://api.currencyfreaks.com/supported-currencies')
+    return response.data
+}
+
+const currencyService = { getCurrencyRates, getCurrencies }
+
+export default currencyService
